5289aa681dd69ebddb6a29d51f122422702a8cf2
hydaar <hyders888@gmail.com>
2022-07-25 12:29:21 +0530

    secondcommit

---
+++ src/test/java/PracticecommandsForGit/secondclassForCommandPracticeTest.java
@@ -0,0 +1,13 @@
+package PracticecommandsForGit;
+
+import org.testng.annotations.Test;
+
+public class secondclassForCommandPracticeTest {
+@Test
+
+	public  void secondsampleTest() {
+		System.out.println("COMMAND PRACTICE CLASS CREATED");
+		System.out.println("COMMAND PRACTICE CLASS CREATED");
+	}
+
+}
